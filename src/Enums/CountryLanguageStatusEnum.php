c9c26f0b4c3d69d4cd5793e026f93bb17ecff2b9
denis <denisprogressive@gmail.com>
2025-06-03 00:48:13 +0200

    fix: more country language status enum

--- src/Enums/CountryLanguageStatusEnum.php
+++ src/Enums/CountryLanguageStatusEnum.php
@@ -9,12 +9,29 @@ namespace Denprog\Meridian\Enums;
  */
 enum CountryLanguageStatusEnum: string
 {
+    // Существующие
     case OFFICIAL = 'official';
     case NATIONAL = 'national';
     case REGIONAL_OFFICIAL = 'regional_official';
     case REGIONAL = 'regional';
     case MINORITY = 'minority';
     case LINGUA_FRANCA = 'lingua_franca';
+    case RECOGNIZED = 'recognized';
+    case WORKING = 'working';
+    case OFFICIAL_ADDITIONAL = 'official_additional';
+    case OFFICIAL_NATIONAL = 'official_national';
+    case OFFICIAL_SECONDARY = 'official_secondary';
+    case SPECIAL_STATUS = 'special_status';
+    case STATE = 'state';
+    case MUNICIPAL_OFFICIAL = 'municipal_official';
+    case ADMINISTRATIVE = 'administrative';
+    case WIDELY_USED = 'widely_used';
+    case DE_FACTO_NATIONAL = 'de_facto_national';
+    case WIDELY_SPOKEN = 'widely_spoken';
+    case MAJOR = 'major';
+    case CO_OFFICIAL = 'co_official';
+    case LINK_LANGUAGE = 'link_language';
+    case INTER_ETHNIC_COMMUNICATION = 'inter_ethnic_communication';
 
     /**
      * Get all enum values.
